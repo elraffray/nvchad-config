26797f156dec499fc93ef2e6d4574935097acfcd
elraffray <elraffray@github.com>
2026-09-22 23:36:03 +0200

    rust: lsp proc-macro support

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,4 +3,20 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls", "rust_analyzer" }
 vim.lsp.enable(servers)
 
+vim.lsp.config('rust_analyzer', {
+  -- Server-specific settings. See `:help lsp-quickstart`
+  settings = {
+    ['rust-analyzer'] = {
+      cargo = {
+        buildScripts = {
+          enable = true
+        }
+      },
+      procMacro = {
+        enable = true
+      }
+    },
+  },
+})
+
 -- read :h vim.lsp.config for changing options of lsp servers
